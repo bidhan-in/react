fc865c00f05e4ec4e4ac526d1c91b886f990157c
Bidhan <bidhan.ssca@gmail.com>
2021-05-29 19:03:37 +0530

    ok

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  let title = 'Hi, Bidhan Ghosh';
+  let title = 'Hi, Bidhan and Aishi Ghosh';
   return (
     <div className="App">
       <h1>{title}</h1>
